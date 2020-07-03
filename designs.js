77ab52c90c5ee800edf84019c3593c648a6efcbf
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2020-07-03 21:12:38 +0100

    Added functions to design.js
    Worked on functions to selst size, color and create the grid

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
+const formItem = document.getElementById("sizePicker");
+formItem.addEventListener("submit", handleSubmit);
+const table = document.getElementById("pixelCanvas").addEventListener('click', setCellColor);
+
 // Select color input
+function setCellColor(e) {
+    e.target.style.backgroundColor = document.getElementById('colorPicker').value;
+}
+
 // Select size input
+function handleSubmit(e) {
+    e.preventDefault();
+    var inputHeight = document.getElementById('inputHeight').value;
+    var inputWidth = document.getElementById("inputWidth").value;
+    makeGrid(inputHeight, inputWidth);
+}
+
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(height, width) {
+    // Your code goes here!
+    const mainTable = document.getElementById('pixelCanvas');
+    mainTable.innerHTML = '';
 
-function makeGrid() {
-
-// Your code goes here!
-
+    for (i = 0; i < height; i++) {
+        const newRow = document.createElement('tr');
+        for (j = 0; j < width; j++) {
+            const newCell = document.createElement('td');
+            newCell.classList.add("clickAble");
+            newRow.appendChild(newCell);
+        }
+        mainTable.appendChild(newRow);
+    }
 }
